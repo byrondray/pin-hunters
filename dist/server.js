d4573918e5167defb266262a09a8fbdf93bf7c7b
byron dray <byrondray8@gmail.com>
2025-01-07 19:05:36 -0800

    fix: build frontend into the dist folder

--- dist/server.js
+++ dist/server.js
@@ -1,17 +1,17 @@
 import { jsx as _jsx } from "jsxte/jsx-runtime";
-import express from "express";
-import path from "path";
-import { expressExtend, renderToHtml } from "jsxte";
-import { CourseList } from "./components/CourseList";
-import { MapPage } from "./components/Map";
-import { db } from "./database/client";
-import { course, hole } from "./database/schema/schema";
-import { eq, sum } from "drizzle-orm";
-import liveReload from "livereload";
-import "dotenv/config";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { configureApp } from "./middleware/express.middleware";
+import express from 'express';
+import path from 'path';
+import { expressExtend, renderToHtml } from 'jsxte';
+import { CourseList } from './components/CourseList';
+import { MapPage } from './components/Map';
+import { db } from './database/client';
+import { course, hole } from './database/schema/schema';
+import { eq, sum } from 'drizzle-orm';
+import liveReload from 'livereload';
+import 'dotenv/config';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { configureApp } from './middleware/express.middleware';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = process.env.PORT || 3000;
 const app = express();
@@ -19,9 +19,9 @@ configureApp(app);
 expressExtend(app);
 const liveReloadServer = liveReload.createServer();
 liveReloadServer.watch(path.join(__dirname));
-liveReloadServer.server.once("connection", () => {
+liveReloadServer.server.once('connection', () => {
     setTimeout(() => {
-        liveReloadServer.refresh("/");
+        liveReloadServer.refresh('/');
     }, 0);
 });
 // app.get("/", async (req: Request, res: Response) => {
@@ -37,44 +37,44 @@ liveReloadServer.server.once("connection", () => {
 //     res.status(500).send("Server error");
 //   }
 // });
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
     try {
         const coursesData = await db
             .select({
             name: course.name,
-            totalPar: sum(hole.par),
+            totalPar: sum(hole.par).as('totalPar'),
         })
             .from(course)
-            .leftJoin(hole, hole.courseId, course.id)
+            .leftJoin(hole, eq(hole.courseId, course.id))
             .groupBy(course.id)
             .execute();
-        console.log("Courses with pars:", coursesData);
+        console.log('Courses with pars:', coursesData);
         const html = renderToHtml(_jsx(CourseList, { courses: coursesData }));
         res.send(html);
     }
     catch (error) {
         console.error(error);
-        res.status(500).send("Server error");
+        res.status(500).send('Server error');
     }
 });
-app.get("/end-session", (req, res) => {
+app.get('/end-session', (req, res) => {
     req.session.selectedCourse = undefined;
     req.session.save((err) => {
         if (err) {
-            console.error("Session save error:", err);
-            res.status(500).send("Server error");
+            console.error('Session save error:', err);
+            res.status(500).send('Server error');
         }
         else {
-            res.redirect("/");
+            res.redirect('/');
         }
     });
 });
-app.get("/get-selected-course", (req, res) => {
+app.get('/get-selected-course', (req, res) => {
     if (req.session.selectedCourse) {
         res.json(req.session.selectedCourse);
     }
 });
-app.post("/select-course", async (req, res) => {
+app.post('/select-course', async (req, res) => {
     const selectedCourseName = req.body.courseName;
     try {
         const selectedCourse = await db
@@ -85,24 +85,24 @@ app.post("/select-course", async (req, res) => {
             req.session.selectedCourse = selectedCourse[0].name;
             req.session.save((err) => {
                 if (err) {
-                    console.error("Session save error:", err);
-                    res.status(500).send("Server error");
+                    console.error('Session save error:', err);
+                    res.status(500).send('Server error');
                 }
                 else {
-                    res.redirect("/map");
+                    res.redirect('/map');
                 }
             });
         }
         else {
-            res.status(404).send("Course not found");
+            res.status(404).send('Course not found');
         }
     }
     catch (error) {
-        console.error("Database query error:", error);
-        res.status(500).send("Server error");
+        console.error('Database query error:', error);
+        res.status(500).send('Server error');
     }
 });
-app.get("/course/:name", async (req, res) => {
+app.get('/course/:name', async (req, res) => {
     try {
         const courseName = req.params.name;
         const selectedCourse = await db
@@ -110,7 +110,7 @@ app.get("/course/:name", async (req, res) => {
             .from(course)
             .where(eq(course.name, courseName));
         if (selectedCourse.length > 0) {
-            console.log(selectedCourse[0], "selectedCourse");
+            console.log(selectedCourse[0], 'selectedCourse');
             const holes = await db
                 .select()
                 .from(hole)
@@ -119,31 +119,31 @@ app.get("/course/:name", async (req, res) => {
                 ...selectedCourse[0],
                 holes: holes,
             };
-            console.log(courseWithHoles, "courseWithHoles");
+            console.log(courseWithHoles, 'courseWithHoles');
             res.json(courseWithHoles);
         }
         else {
-            res.status(404).send("Course not found");
+            res.status(404).send('Course not found');
         }
     }
     catch (error) {
-        console.error("Database query error:", error);
-        res.status(500).send("Server error");
+        console.error('Database query error:', error);
+        res.status(500).send('Server error');
     }
 });
-app.get("/map", (req, res) => {
+app.get('/map', (req, res) => {
     try {
         if (req.session.selectedCourse) {
             const html = renderToHtml(_jsx(MapPage, {}));
             res.send(html);
         }
         else {
-            res.redirect("/");
+            res.redirect('/');
         }
     }
     catch (error) {
-        console.error("Error occurred when trying to render /map:", error);
-        res.status(500).send("Server error");
+        console.error('Error occurred when trying to render /map:', error);
+        res.status(500).send('Server error');
     }
 });
 app.listen(port, () => {
